22dd9971efc56e4cf8701b6f6f54f292425b162d
Maximilian Albert <maximilian.albert@flowminder.org>
2018-03-24 17:19:15 +0000

    Move sample_data to toplevel

--- chantstats/tests/unit_tests/test_load_musicxml_file.py
+++ chantstats/tests/unit_tests/test_load_musicxml_file.py
@@ -4,7 +4,7 @@ from .context import chantstats
 from chantstats.io import load_musicxml_file
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-SAMPLE_DATA_DIR = os.path.join(HERE, '..', 'sample_data')
+SAMPLE_DATA_DIR = os.path.join(HERE, '..', '..', 'sample_data')
 
 
 def test_load_musicxml_file():
