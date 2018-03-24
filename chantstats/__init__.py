f702b02f94b632b7c99ecad446a8f59419a84b44
Maximilian Albert <maximilian.albert@flowminder.org>
2018-03-24 14:01:05 +0000

    Add setup.py file and set up version management with versioneer

---
+++ chantstats/__init__.py
@@ -0,0 +1,4 @@
+# Version management provided by versioneer (https://github.com/warner/python-versioneer)
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
