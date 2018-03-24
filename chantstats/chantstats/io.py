6b917571cc00c1ba9e8c0d8fd853c0f097f581aa
Maximilian Albert <maximilian.albert@flowminder.org>
2018-03-24 17:57:31 +0000

    Add convenience function to load sample MusicXML file

--- chantstats/chantstats/io.py
+++ chantstats/chantstats/io.py
@@ -1,4 +1,8 @@
 import music21
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+SAMPLE_DATA_DIR = os.path.join(HERE, '..', 'sample_data')
 
 
 def load_musicxml_file(filename):
@@ -14,4 +18,22 @@ def load_musicxml_file(filename):
     -------
     music21.stream.Stream
     """
+    if not os.path.exists(filename):
+        raise IOError("File does not exist: '{}'".format(filename))
     return music21.converter.parse(filename)
+
+
+def load_sample_file(filename):
+    """
+    Load MusicXML file from the 'sample_data' folder.
+
+    Parameters
+    ----------
+    filename: str
+        Filename of the MusicXML file. This should not contain any directory components.
+
+    Returns
+    -------
+    music21.stream.Stream
+    """
+    return load_musicxml_file(os.path.join(SAMPLE_DATA_DIR, filename))
--- chantstats/tests/unit_tests/test_load_musicxml_file.py
+++ chantstats/tests/unit_tests/test_load_musicxml_file.py
@@ -1,14 +1,36 @@
 import music21
 import os
+import pytest
 from .context import chantstats
-from chantstats.io import load_musicxml_file
+from chantstats.io import load_musicxml_file, load_sample_file
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 SAMPLE_DATA_DIR = os.path.join(HERE, '..', '..', 'sample_data')
 
 
 def test_load_musicxml_file():
+    """
+    Result of loading sample MusicXML file is a music21 stream
+    """
     filename = os.path.join(SAMPLE_DATA_DIR, '01_Salus_eterna.xml')
     chant = load_musicxml_file(filename)
+    notes = [n.nameWithOctave for n in chant.flat.notes]
 
     assert isinstance(chant, music21.stream.Stream)
+    assert notes[:5] == ['B3', 'D4', 'E4', 'B3', 'G3']
+
+
+def test_load_sample_file():
+    """
+    Result of loading sample MusicXML file is a music21 stream
+    """
+    chant = load_sample_file('01_Salus_eterna.xml')
+    notes = [n.nameWithOctave for n in chant.flat.notes]
+
+    assert isinstance(chant, music21.stream.Stream)
+    assert notes[:5] == ['B3', 'D4', 'E4', 'B3', 'G3']
+
+
+def test_load_musixml_file_raises_IOError_if_file_doesn_not_exist():
+    with pytest.raises(IOError):
+        load_musicxml_file('Foobar.xml')
