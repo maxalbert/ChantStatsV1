89fce4a7edf6e74d53c070c5feb11da8504e23f2
Maximilian Albert <maximilian.albert@flowminder.org>
2018-03-26 21:18:48 +0100

    Add select element to allow choosing chant

--- chantstats/chantstats/static/js/ChantSelect.jsx
+++ chantstats/chantstats/static/js/ChantSelect.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 const options = [
-  "01_Salus_eterna.xml",
-  "02_Regnantem.xml",
-  "03_Qui_regis.xml",
+  {value: "01_Salus_eterna.xml", label: "01 Salus eterna"},
+  {value: "02_Regnantem.xml", label: "02 Regnantem"},
+  {value: "03_Qui_regis.xml", label: "03 Qui regis"},
 ]
 
 export default class ChantSelect extends React.Component {
-  constructor (props) {
-    super(props);
+  state = {
+    selectedOption: '',
   }
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Selected: ${selectedOption.label}`);
+  }
+  render() {
+    const { selectedOption } = this.state;
+    const value = selectedOption && selectedOption.value;
 
-  render () {
     return (
-      <div>
-        <h3>Select your chants:</h3>
+      <div style={ {width: '400px'} }>
+        <Select
+          name="form-field-name"
+          value={value}
+          onChange={this.handleChange}
+          options={options}
+        />
       </div>
     );
   }
